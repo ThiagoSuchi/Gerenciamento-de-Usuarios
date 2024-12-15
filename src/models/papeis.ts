19bee406cb3756ac5a0ccd423f58f876a75436ed
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-12-14 23:13:53 -0400

    Projeto Gerenciador de Usuários - problemas ao filtrar por id, pois o id é do tipo uuidv4()...

--- src/models/papeis.ts
+++ src/models/papeis.ts
@@ -3,3 +3,20 @@ export enum Papeis {
     Convidado = 'Convidado',
     Professor = 'Professor'
 }
+
+const  listPermissaoConvidado = [
+    'Listar produtos', 
+    'Listar produtos por id'
+];
+const listPermissaoAdmin = [
+    'Listar produtos',
+    'Listar produtos por id',
+    'Cadastrar produtos',
+    'Alterar produtos',
+    'Deletar produtos'
+];
+
+export const permissaoUserPapel = {
+    [Papeis.Administrador]: listPermissaoAdmin,
+    [Papeis.Convidado]: listPermissaoConvidado
+}
--- src/services/usuarioService.ts
+++ src/services/usuarioService.ts
@@ -1,8 +1,42 @@
-import { Papeis } from "../models/papeis";
+import bcrypt from 'bcrypt';
 import { Usuario } from "../models/usuario";
 import { usuario } from "../seeds/usuarioSeeds";
-import { v4 as uuidv4 } from "uuid"
 import { salvarArquivo } from "./csvService";
+
+// cadastra um novo usuário
+export const cadastrarUsuario = async (usuarios: Usuario[], usuario: Usuario) => {
+    // Validação do nome
+    if (usuario.nome.length < 3 || usuario.nome.length > 25) {
+        return 'Nome inválido, o nome deve ter no mínimo 3 e no máximo 25 caracteres.';
+    }
+
+    // Validação de email
+    const validEmail = (email: string): boolean => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email)
+    }
+    
+    if(!validEmail(usuario.email)) {
+        return 'Este email é inválido - exemplo de email: exemple@gmail.com';
+    }
+
+    // Validação de senha
+    const validSenha = (senha: string): boolean => {
+        const senhaRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s])[A-Za-z\d\W]{8,}$/;
+        return senhaRegex.test(senha)
+    }
+    
+    if(!validSenha(usuario.senha)) {
+        return 'Senha inválida, sua senha deve conter no mínimo 8 caracteres, letras maiúsculas, letras minúsculas, números e caracteres especiais.';
+    }
+
+    const saltRound = 10;
+    usuario.senha = await bcrypt.hash(usuario.senha, saltRound)
+    
+    usuarios.push(usuario);
+    salvarArquivo(usuarios)
+    return usuarios;
+}
 
 // Listando os usuários de teste
 export const listarUsuarios = (usuarios: Usuario[]): void => {
@@ -12,24 +46,12 @@ export const listarUsuarios = (usuarios: Usuario[]): void => {
     });
 };
 
-export const cadastrarUsuario = (usuarios: Usuario[], usuario: Usuario)  => {
-    usuarios.push(usuario);
-    return usuarios;    
-}
 
-const novoUsuario: Usuario = {
-    id: uuidv4(),
-    nome: 'Valderio',
-    email: 'valde@gmail.com',
-    senha: 'valDERIO123321#',
-    papel: Papeis.Convidado,
-    dataCadastro: new Date(),
-    dataUltimaAlteracao: new Date(),
-    status: "inativo"    
+export const listUsuarioId = (usuario: Usuario[], idPassado: string) => {
+    return usuario.find(user => user.id === idPassado);
 }
 
 listarUsuarios(usuario)
-console.log('Cadastrando um novo usuário');
-cadastrarUsuario(usuario, novoUsuario)
-listarUsuarios(usuario)
-salvarArquivo(usuario)
+// resolver id, esta retornando undefined
+console.log(listUsuarioId(usuario, 'dd55b159-9e63-44c8-adf0-7db7d86cceca'));
+
